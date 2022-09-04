f4a2fd21aa23e739cf7c5077004e627d27b1f030
james etchberger <etchbergerjames@gmail.com>
2022-09-04 10:34:00 -0400

    made grammatical changes to the about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -5,7 +5,7 @@ export default function About () {
         <div className="aboutme">
         <h1>About me</h1>
         <br />
-        <p>My name is James Etchberger. I Lived in Mexico City, Mexico until i was almost 18 years old. After several years in the U.S. i found my love for the kitchen and making amazing food. I worked in the culinary industry for almost a decade. I pushed myself hard and ended up as a Sous Chef on my way to becoming an Executive chef. I was in a car accident in early 2021 and it changed my life. From the wounds of that accident I had to give up my dreams of working in kitchens. I had always been very interested in the tech world and the possibilities in this industry. Now after a year and a half of recovery I have begun my journey in tech. I built this website to combine both of my passions, the kitchen and tech. Please enjoy some of my personal recipes and use this site to store your very own.</p>
+        <p>My name is James Etchberger. I lived in Mexico City, Mexico until I was almost 18 years old. After several years in the U.S., I found my love for the kitchen and making amazing food. I worked in the culinary industry for almost a decade. I pushed myself hard and ended up as a Sous Chef on my way to becoming an Executive Chef. I was in a car accident in early 2021 and it changed my life. From the wounds of that accident I had to give up my dreams of working in kitchens. I had always been very interested in the tech world and the possibilities in this industry. Now after a year and a half of recovery, I have begun my journey in tech. I built this website to combine both of my passions, the kitchen and tech. Please enjoy some of my personal recipes and use this site to store your very own.</p>
         </div>
 
     )
