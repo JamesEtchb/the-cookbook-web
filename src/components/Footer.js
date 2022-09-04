e3f1dfeada9de5868fa22b5ffb05657f32da2c0d
james etchberger <etchbergerjames@gmail.com>
2022-09-04 10:28:24 -0400

    changed the about me sizing to fix mobile issues

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
-export default function Footer () {
-    return (
-        <h4>© Made by James 2022</h4>
-    )
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <h4>© Made by James 2022</h4>
+    </div>
+  )
 }
