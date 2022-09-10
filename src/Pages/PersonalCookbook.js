995f1a07c4020ff8a4486e8484b39a9de35ac0a4
james etchberger <etchbergerjames@gmail.com>
2022-09-10 13:08:49 -0400

    made some css changes to presonal page

--- src/Pages/PersonalCookbook.js
+++ src/Pages/PersonalCookbook.js
@@ -1,37 +1,47 @@
-import InputForm from "../components/Input";
-import FoodCarousel from "../components/Carousel";
-import { useState, useEffect } from "react";
+import InputForm from '../components/Input'
+import FoodCarousel from '../components/Carousel'
+import { useState, useEffect } from 'react'
 import './Personal.css'
 
-export default function PersonalCookbook () {
-    const [showFood, setShowFood] = useState([])
-    const [token, setToken] = useState('')
-    useEffect(() => {
-        const _token = localStorage.getItem('token')
-        if(_token) {
-            setToken(_token)
-        }
-    }, [])
-    useEffect(() => {
-        fetch('https://the-cookbook-api.web.app/personal')
-        .then((results) => results.json())
-        .then((data) => setShowFood(data))
-        .catch(console.error)
-    }, [setShowFood])
-    if (!showFood) {
-        <h2>No personal recipes posted</h2>
+export default function PersonalCookbook() {
+  const [showFood, setShowFood] = useState([])
+  const [token, setToken] = useState('')
+  useEffect(() => {
+    const _token = localStorage.getItem('token')
+    if (_token) {
+      setToken(_token)
     }
-    return (
-        <div>
-            {!token ? <h1>Please Login to use your personal cookbook</h1> : 
-            <>
-        <div className="personal">
-            <h1>Welcome to your personal cookbook</h1>
-            <h3>Please enjoy filling out your own recipes and being sure they are safe.</h3>
-            <FoodCarousel showFood={showFood} />
-        </div>
-            <InputForm setShowFood={setShowFood} />
-            </>}
-        </div>
-    )
+  }, [])
+  useEffect(() => {
+    fetch('https://the-cookbook-api.web.app/personal')
+      .then((results) => results.json())
+      .then((data) => setShowFood(data))
+      .catch(console.error)
+  }, [setShowFood])
+  if (!showFood) {
+    ;<h2>No personal recipes posted</h2>
+  }
+  return (
+    <>
+      <div className="personal">
+        <h1>Welcome to your personal cookbook</h1>
+        <h3>
+          Please enjoy filling out your own recipes and enjoying them forever.
+        </h3>
+        <FoodCarousel showFood={showFood} />
+      </div>
+      <InputForm setShowFood={setShowFood} />
+    </>
+    // <div>
+    //     {!token ? <h1>Please Login to use your personal cookbook</h1> :
+    //     <>
+    // <div className="personal">
+    //     <h1>Welcome to your personal cookbook</h1>
+    //     <h3>Please enjoy filling out your own recipes and being sure they are safe.</h3>
+    //     <FoodCarousel showFood={showFood} />
+    // </div>
+    //     <InputForm setShowFood={setShowFood} />
+    //     </>}
+    // </div>
+  )
 }
